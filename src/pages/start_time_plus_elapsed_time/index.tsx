c88a830766affa32c4910b1a2023a4b54cfb90c9
aoirint <aoirint@gmail.com>
2023-08-30 20:03:52 +0900

    outputTimeLines placeholder

--- src/pages/start_time_plus_elapsed_time/index.tsx
+++ src/pages/start_time_plus_elapsed_time/index.tsx
@@ -7,6 +7,8 @@ import {
   Button,
   FormControl,
   MenuItem,
+  Grid,
+  Stack,
 } from '@mui/material'
 import { formatISO } from 'date-fns'
 import Head from 'next/head'
@@ -19,6 +21,7 @@ interface StartTimePlusElapsedTimePageFormValues {
   startTime: string
   outputTimezone: string
   elapsedTimeLines: string
+  outputTimeLines: string
 }
 
 export default function StartTimePlusElapsedTimePage() {
@@ -31,6 +34,7 @@ export default function StartTimePlusElapsedTimePage() {
       startTime: currentDateString,
       outputTimezone: currentTimezone,
       elapsedTimeLines: '',
+      outputTimeLines: '',
     },
   })
 
@@ -50,81 +54,113 @@ export default function StartTimePlusElapsedTimePage() {
         <Box sx={{ mt: 3 }}>
           <form onSubmit={handleSubmit(onSubmit)}>
             <Box sx={{ mt: 3 }}>
-              <FormControl sx={{ width: '40ch' }}>
-                <Controller
-                  name='startTime'
-                  control={control}
-                  render={({ field }) => (
-                    <TextField
-                      label='開始日時'
-                      placeholder={currentDateString}
-                      helperText='ISO 8601 (RFC 3339) 拡張形式のタイムゾーン付き日時'
-                      {...field}
-                    />
-                  )}
-                />
-              </FormControl>
-              <Button
-                onClick={() => {
-                  const currentDateString = formatISO(new Date())
+              <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 2, sm: 2 }}
+                alignItems={{ xs: 'stretch', sm: 'center' }}
+              >
+                <FormControl sx={{ width: '40ch' }}>
+                  <Controller
+                    name='startTime'
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        label='開始日時'
+                        placeholder={currentDateString}
+                        helperText='ISO 8601 (RFC 3339) 拡張形式のタイムゾーン付き日時'
+                        {...field}
+                      />
+                    )}
+                  />
+                </FormControl>
+                <Button
+                  onClick={() => {
+                    const currentDateString = formatISO(new Date())
 
-                  setValue('startTime', currentDateString)
-                }}
-                variant='contained'
-                sx={{ ml: 2, mt: 1 }}
-              >
-                現在時刻を入力
-              </Button>
+                    setValue('startTime', currentDateString)
+                  }}
+                  variant='contained'
+                  sx={{ ml: 2, mt: 1 }}
+                >
+                  現在時刻を入力
+                </Button>
+              </Stack>
             </Box>
-            <Box sx={{ mt: 3 }}>
-              <FormControl sx={{ width: '40ch' }}>
-                <Controller
-                  name='outputTimezone'
-                  control={control}
-                  render={({ field }) => (
-                    <TextField
-                      select
-                      label='出力のタイムゾーン'
-                      placeholder={currentTimezone}
-                      helperText='IANA タイムゾーン識別子'
-                      {...field}
-                    >
-                      {availableTimezones.map((timezone) => (
-                        <MenuItem key={timezone} value={timezone}>
-                          {timezone}
-                        </MenuItem>
-                      ))}
-                    </TextField>
-                  )}
-                />
-              </FormControl>
-              <Button
-                onClick={() => {
-                  setValue('outputTimezone', currentTimezone)
-                }}
-                variant='contained'
-                sx={{ ml: 2, mt: 1 }}
+            <Box sx={{ mt: 4 }}>
+              <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 2, sm: 2 }}
+                alignItems={{ xs: 'stretch', sm: 'center' }}
               >
-                現在のタイムゾーンを入力
-              </Button>
+                <FormControl sx={{ width: '40ch' }}>
+                  <Controller
+                    name='outputTimezone'
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        select
+                        label='出力のタイムゾーン'
+                        placeholder={currentTimezone}
+                        helperText='IANA タイムゾーン識別子'
+                        {...field}
+                      >
+                        {availableTimezones.map((timezone) => (
+                          <MenuItem key={timezone} value={timezone}>
+                            {timezone}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    )}
+                  />
+                </FormControl>
+                <Button
+                  onClick={() => {
+                    setValue('outputTimezone', currentTimezone)
+                  }}
+                  variant='contained'
+                  sx={{ ml: 2, mt: 1 }}
+                >
+                  現在のタイムゾーンを入力
+                </Button>
+              </Stack>
             </Box>
-            <Box sx={{ mt: 3 }}>
-              <FormControl sx={{ width: '40ch' }}>
-                <Controller
-                  name='elapsedTimeLines'
-                  control={control}
-                  render={({ field }) => (
-                    <TextField
-                      multiline
-                      minRows={3}
-                      label='経過時間'
-                      placeholder={`00:12:34\n01:23:45`}
-                      helperText='HH:mm:ss 形式の経過時間（複数行）'
-                      {...field}
-                    />
-                  )}
-                />
-              </FormControl>
+            <Box sx={{ mt: 4 }}>
+              <Stack direction={{ xs: 'column', md: 'row' }} spacing={2}>
+                <FormControl sx={{ width: '40ch' }}>
+                  <Controller
+                    name='elapsedTimeLines'
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        multiline
+                        minRows={3}
+                        label='経過時間'
+                        placeholder={`00:12:34\n01:23:45`}
+                        helperText='HH:mm:ss 形式の経過時間（複数行）'
+                        {...field}
+                      />
+                    )}
+                  />
+                </FormControl>
+                <FormControl sx={{ width: '40ch' }}>
+                  <Controller
+                    name='outputTimeLines'
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        multiline
+                        minRows={3}
+                        label='出力時間'
+                        placeholder={`${currentDateString}\n${currentDateString}`}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        {...field}
+                      />
+                    )}
+                  />
+                </FormControl>
+              </Stack>
             </Box>
           </form>
         </Box>
