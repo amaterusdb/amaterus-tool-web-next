8679f65ace08224538c4178a8dbd0e926accaadb
aoirint <aoirint@gmail.com>
2023-08-30 19:11:49 +0900

    start time input

--- src/pages/start_time_plus_elapsed_time/index.tsx
+++ src/pages/start_time_plus_elapsed_time/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Toolbar, Typography, Link as MuiLink, TextField } from '@mui/material'
+import { Box, Toolbar, Typography, Link as MuiLink, TextField, Button, FormControl } from '@mui/material'
 import Head from 'next/head'
 import NextLink from 'next/link'
 import { useForm, Controller } from 'react-hook-form'
@@ -6,12 +6,14 @@ import DrawerAppBar from '@/components/drawer_app_bar'
 
 interface StartTimePlusElapsedTimePageFormValues {
   startTime: string
+  elapsedTimeLines: string
 }
 
 export default function StartTimePlusElapsedTimePage() {
-  const { control, handleSubmit } = useForm<StartTimePlusElapsedTimePageFormValues>({
+  const { control, handleSubmit, setValue } = useForm<StartTimePlusElapsedTimePageFormValues>({
     defaultValues: {
-      startTime: '', // TODO:
+      startTime: '',
+      elapsedTimeLines: '',
     },
   })
 
@@ -26,14 +28,30 @@ export default function StartTimePlusElapsedTimePage() {
       <Box component='main' sx={{ p: 3 }}>
         <Toolbar />
         <Typography variant='h4' component='h2'>
-          Amaterus Tool
+          開始日時＋経過時間
         </Typography>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name='startTime'
-            control={control}
-            render={({ field }) => <TextField label='開始日時' variant='standard' {...field} />}
-          />
+          <Box sx={{ mt: 3 }}>
+            <FormControl sx={{ width: '27.5ch' }}>
+              <Controller
+                name='startTime'
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    label='開始日時'
+                    placeholder='2023-01-01T00:00:00+09:00'
+                    helperText='ISO 8601 (RFC 3339)の時刻形式'
+                    {...field}
+                  />
+                )}
+              />
+            </FormControl>
+            <Button onClick={() => {
+              setValue("startTime", "")
+            }} variant='contained' sx={{ ml: 2, mt: 1 }}>
+              現在時刻を入力
+            </Button>
+          </Box>
         </form>
       </Box>
     </>
